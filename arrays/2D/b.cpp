69252db1230f1945cc0349f232651dee23a8ff2f
Himanshu Poptani <himanshupoptani12@gmail.com>
2021-11-04 15:24:16 +0530

    Day2

--- arrays/2D/b.cpp
+++ arrays/2D/b.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+// Searching in matrix
+
+#include<iostream>
 using namespace std;
 
 int main(){
@@ -12,6 +14,18 @@ int main(){
                         cin>>arr[i][j];
                 }
         }
+
+        int key;
+        cin>>key;
+
+        // Searching in matrix
+       for(int i=0; i<n; i++){
+               for(int j=0; j<n; j++){
+                       if(arr[i][j]==key){
+                               cout<<"Key is at: "<<"Row: "<<i<<" Column: "<<j<<endl;
+                       }
+               }
+       }
 
         // Printing output
         for(int i=0; i<n; i++){
